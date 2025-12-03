f0e2490228b63f0b282377ff26dd72b47f502925
Carson1829 <cchiem2@gmail.com>
2025-12-02 22:39:08 -0800

    updated main.py for glove

--- main.py
+++ main.py
@@ -5,26 +5,34 @@ from torch.utils.data import DataLoader
 from model import RNNs
 from train import train_model
 from sklearn.model_selection import train_test_split
-from evaluation import evaluate
+from evaluation import get_preds
+import argparse
+
+'''
+Best Val F1: 0.8432432432432432
+Best Config: {'model_type': 'lstm', 'hidden_dim': 256, 'dropout': 0.1, 'num_layers': 2, 'attention_heads': 0}
+'''
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_path")
+    parser.add_argument("test_path")
+    parser.add_argument("output_csv")
+    args = parser.parse_args()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load CSVs
-    initial_df = pd.read_csv("data/train.csv")
-    test_data = pd.read_csv("data/test.csv")
-
-    train_data, val_data = train_test_split(initial_df,random_state = 32, test_size = 0.25, shuffle = True)
+    train_data = pd.read_csv(args.train_path)
+    test_data = pd.read_csv(args.test_path)
 
     # Load sentences and tags
     train_sents, train_tags = load_data(train_data, has_tags=True)
-    val_sents, val_tags = load_data(val_data, has_tags=True)
     test_sents, _ = load_data(test_data, has_tags=False)
 
     # Tokenize
     tokenized_train = tokenize(train_sents)
-    tokenized_val = tokenize(val_sents)
     tokenized_test = tokenize(test_sents)
 
     # Build vocabularies
@@ -33,26 +41,24 @@ def main():
 
     # Create datasets
     train_dataset = DS(tokenized_train, train_tags, word2idx=word2idx, tag2idx=tag2idx)
-    val_dataset = DS(tokenized_val, val_tags, word2idx=word2idx, tag2idx=tag2idx)
     test_dataset = DS(tokenized_test, tags=None, word2idx=word2idx)
 
     # create DataLoader
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
 
     # create model
     vocab_size = len(word2idx)
     tag_size = len(tag2idx)
-    model = RNNs(vocab_size=vocab_size, tag_size=tag_size, model_type="lstm", attention_heads=2)
+    emb_matrix = torch.load("embedding.pt")
+    model = RNNs(vocab_size=vocab_size, tag_size=tag_size, model_type="lstm", attention_heads=0, pretrained_emb=emb_matrix)
 
     # train
-    model = train_model(model, train_loader, tag2idx, epochs=10)
+    model = train_model(model, train_loader, tag2idx, epochs=18)
     
-    f1, outputs = evaluate(model, val_loader, tag_list, device)
-    print("F1:", f1)
+    outputs = get_preds(model, test_loader, tag_list, device)
     output_df = pd.DataFrame(outputs, columns=["ID","IOB Slot tags"])
-    # output_df.to_csv("test_pred.csv", index=False)
+    output_df.to_csv(args.output_csv, index=False)
     print(f"Predictions saved to csv")
 
 if __name__ == "__main__":
